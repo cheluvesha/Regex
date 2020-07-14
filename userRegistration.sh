6a3e86b3b92d9ef1ff3559b37c855ba309bfbcaa
cheluvesha <cheluvesh.b@gmail.com>
2020-07-14 19:44:08 +0530

    password with uppercase validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
-read -p "Enter Email to Check: " email
-patternemail="^[A-Za-z+.-]+(\\.[0-9]+)*@[A-Za-z]+(\\.[A-Za-z]+)*(\\.[A-Za-z]{2,})?$"
-if [[ $email =~ $patternemail ]]
-then
-        echo "Valid"
-else
-        echo "Not Valid"
+read -p "Enter your password:" password
+patternForPassword="^[A-Z]{1}[a-z]{8,}$"
+if [[ $password =~ $patternForPassword ]]
+        then
+                echo Valid
+        else
+                echo Invalid
 fi
