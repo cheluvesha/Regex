c83a6f462764cb86c60d05299b9b424ce4b73948
cheluvesha <cheluvesh.b@gmail.com>
2020-07-14 18:57:40 +0530

    Valid email is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,12 @@
-#!bin/bash
-read -p "Enter the last name:" lastName
-patternForLastName="^[A-Z]{1}[a-z]{2,}$"
-if [[ $lastName =~ $patternForLastName ]]
-then
-        echo Valid
-else
-        echo Invalid
+#!/bin/bash -x
+shopt -s extglob
+read -p "Enter your email id:" emailId
+patternForEmailId="^[a-zA-Z]([.]{0,1}[a-zA-Z0-9]*){0,1}[@]{0,1}[a-z]{2,}([.]{0,1}[a-z]{3,})([.][a-bxy])?"
+if [[ $emailId =~ $patternForEmailId ]]
+        then
+                echo Valid
+        else
+                echo Invalid
 fi
+echo $result
 
