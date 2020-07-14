c90bade794e92c3ccd0ce173f022b6f640e4fb2c
cheluvesha <cheluvesh.b@gmail.com>
2020-07-14 19:25:18 +0530

    password with special character is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "Enter your password:" password
-PatternForPassword="^[0-9A-Za-z]+[0-9]*[A-Za-z0-9]*$"
+patternForPassword="^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$"
 if [[ $password =~ $patternForPassword ]]
 then
         echo Valid
 else
         echo Invalid
 fi
+
+
+
+
