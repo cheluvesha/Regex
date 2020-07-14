f3d3dc30077e359a210be2bd8870df001b6f09f0
cheluvesha <cheluvesh.b@gmail.com>
2020-07-14 19:11:27 +0530

    Password minimum 8 chars

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -x
-COUNTRY_CODE=91
-read -p "Enter your mobile number:" mobileNumber
-patternForMobileNumber="^$COUNTRY_CODE[ ]{0,1}[6-9]{1}[0-9]{9}"
-if [[ $mobileNumber =~ $patternForMobileNumber ]]
+read -p "Enter your password:" password
+patternForPassword="^.{8}$"
+if [[ $password =~ $patternForPassword ]]
         then
                 echo Valid
         else
                 echo Invalid
 fi
-
-
-
 
