28f8a822d2f01227a4039e5ef6034be2125bc9c8
cheluvesha <cheluvesh.b@gmail.com>
2020-07-14 19:15:07 +0530

    UPPER case in password validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "Enter your password:" password
-patternForPassword="^.{8}$"
+patternForPassword="^[A-Z]{1}[a-z]{8,}$"
 if [[ $password =~ $patternForPassword ]]
         then
                 echo Valid
