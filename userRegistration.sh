04ac1957ae1adf44d2365398d7b2bb673c07a1bc
cheluvesha <cheluvesh.b@gmail.com>
2020-07-15 11:23:48 +0530

    validation for Last name is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#UC-1
+read -p "Enter the first name:" firstName
+patternForFirstName="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $firstName =~ $patternForFirstName ]]
+then
+        echo Valid
+else
+        echo Invalid
+fi
+#UC-2
 read -p "Enter the last name:" lastName
 patternForLastName="^[[:upper:]]{1}[[:lower:]]{2,}$"
 if [[ $lastName =~ $patternForLastName ]]
