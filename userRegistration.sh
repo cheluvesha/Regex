2a5bdafdc361f0b716a3bf505c599d44bcddc767
cheluvesha <cheluvesh.b@gmail.com>
2020-07-15 11:28:35 +0530

    Validation for Email is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -x
+#UC-1
 shopt -s extglob
+read -p "Enter the first name:" firstName
+patternForFirstName="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $firstName =~ $patternForFirstName ]]
+then
+        echo Valid
+else
+        echo Invalid
+fi
+#UC-2
+read -p "Enter the last name:" lastName
+patternForLastName="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $lastName =~ $patternForLastName ]]
+then
+        echo Valid
+else
+        echo Invalid
+fi
+#UC-3
 read -p "Enter your email id:" emailId
 patternForEmailId="^[a-zA-Z]([.]{0,1}[a-zA-Z0-9]*){0,1}[@]{0,1}[a-z]{2,}([.]{0,1}[a-z]{3,})([.][a-bxy])?"
 if [[ $emailId =~ $patternForEmailId ]]
