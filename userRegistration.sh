1fdaa40f55cde05ceeefff7f50b5f46436144de0
cheluvesha <cheluvesh.b@gmail.com>
2020-07-15 12:04:27 +0530

    Password Validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,52 +1,46 @@
-#!/bin/bash -x
-function validFirstname() { 
+#UC-1
+shopt -s extglob
 read -p "Enter the first name:" firstName
 patternForFirstName="^[[:upper:]]{1}[[:lower:]]{2,}$"
 if [[ $firstName =~ $patternForFirstName ]]
-        then
-                echo "Valid Firstname"
-        else
-        echo "Invalid Firstname Please give an appropriate one"
+then
+        echo Valid
+else
+        echo Invalid
 fi
-}
-
-function validLastname() {
+#UC-2
 read -p "Enter the last name:" lastName
 patternForLastName="^[[:upper:]]{1}[[:lower:]]{2,}$"
 if [[ $lastName =~ $patternForLastName ]]
-        then
-                 echo "Valid Lastname"
-        else
-                echo "Invalid Lastname Please give an appropriate one"
+then
+        echo Valid
+else
+        echo Invalid
 fi
-}
-
-function validEmail() {
-read -p "Enter your email id ex: abc.xyz.bl.com :" emailId
+#UC-3
+read -p "Enter your email id:" emailId
 patternForEmailId="^[a-zA-Z]([.]{0,1}[a-zA-Z0-9]*){0,1}[@]{0,1}[a-z]{2,}([.]{0,1}[a-z]{3,})([.][a-bxy])?"
 if [[ $emailId =~ $patternForEmailId ]]
         then
-                echo "Valid Email"
+                echo Valid
         else
-                echo "Invalid Email"
+                echo Invalid
 fi
-
-}
-function validMobileNumber() {
+#UC-4
 COUNTRY_CODE=91
 read -p "Enter your mobile number:" mobileNumber
 patternForMobileNumber="^$COUNTRY_CODE[ ]{0,1}[6-9]{1}[0-9]{9}"
 if [[ $mobileNumber =~ $patternForMobileNumber ]]
         then
-                echo "Valid Mobile Number"
+                echo Valid
         else
-                echo "Invalid Mobile Number"
+                echo Invalid
 fi
-}
-function validPassword() {
+
+#UC-5,6,7
 read -p "Enter your password:" password
 patternForPassword="^.{8,}"
-patternForPassword2="[:upper:]{1,}"
+patternForPassword2="[[:upper:]]+"
 patternForPassword3="[0-9]{1,}"
 patternForPassword4="^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$"
 if [[ $password =~ $patternForPassword ]]
@@ -70,10 +64,4 @@ then
 else
         echo "Invalid password. password should be of minimum 8 characters"
 fi
-}
-validFirstname
-validLastname
-validEmail
-validMobileNumber
-validPassword
 
