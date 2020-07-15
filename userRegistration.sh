330fbd8b337b2046b651b49c2643a4dc319407d5
cheluvesha <cheluvesh.b@gmail.com>
2020-07-15 09:26:44 +0530

    valid Lastname is readded

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "Enter the first name:" firstName
-patternForFirstName="^[[ :upper: ]]{1}[[ :lower: ]]{2,}$"
+patternForFirstName="^[[:upper:]]{1}[[:lower:]]{2,}$"
 if [[ $firstName =~ $patternForFirstName ]]
 then
         echo Valid
